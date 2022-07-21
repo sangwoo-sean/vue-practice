16543cdb06d63554b6b39a3cb02d21d9ef5a5306
sangwoo <sw.park1@okestro.com>
2022-07-21 13:46:36 +0900

    todo v1

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,17 @@
 <template>
-  <div id="app">
-    <nav>
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </nav>
-    <router-view/>
+  <div id="app" class="container">
+    <h1>To Do</h1>
+    <input type="text" class="w-100 p-3" placeholder="Enter Todo" @keyup.enter="addTodo">
+    <hr>
+    <TodoItem
+        v-for="todo in todos"
+        :key="todo.id"
+        :todo="todo"
+        @toggle-checkbox="toggleCheckbox"
+        @deleteTodo="deleteTodo"
+    >
+      {{ todo }}
+    </TodoItem>
   </div>
 </template>
 
@@ -30,3 +37,47 @@ nav a.router-link-exact-active {
   color: #42b983;
 }
 </style>
+<script>
+import TodoItem from "@/components/TodoItem";
+
+export default {
+  components: {TodoItem},
+  data() {
+    return {
+      todos: [
+        {
+          id: 1,
+          text: 'buy a car',
+          checked: false
+        },
+        {
+          id: 2,
+          text: 'buy a house',
+          checked: true
+        }
+      ]
+    }
+  },
+  methods: {
+    addTodo(e) {
+      const text = e.currentTarget.value;
+      const todoItem = {
+        id: Math.random(),
+        text,
+        checked: false
+      };
+      this.todos.push(todoItem);
+
+      e.currentTarget.value = '';
+    },
+    toggleCheckbox({id, checked}) {
+      const target = this.todos.find(todo => todo.id === id);
+      target.checked = checked;
+    },
+    deleteTodo(id) {
+      this.todos = this.todos.filter(todo => todo.id !== id);
+    },
+  },
+
+}
+</script>
---
+++ src/components/TodoItem.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <input type="checkbox" @change="toggleCheckbox" :checked="todo.checked">
+    <span
+        class="m-2"
+        :class="todo.checked ? 'text-muted' : ''"
+        :style="todo.checked ? 'text-decoration: line-through;' : ''"
+    >{{ todo.text }}</span>
+    <button class="btn btn-sm"
+            @click="$emit('deleteTodo', todo.id)"
+    >❌</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "TodoItem",
+  props: {
+    todo: {
+      type: Object
+    },
+  },
+  methods: {
+    toggleCheckbox(e) {
+      this.$emit('toggle-checkbox', {
+        id: this.todo.id,
+        checked: e.target.checked
+      })
+    },
+  },
+}
+</script>
+
+<style scoped>
+
+</style>
